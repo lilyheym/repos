ad27c9490b8344d2c68f4282533c861c5ac07165
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-11-28 14:29:45 -0800

    Initial commit

---
+++ VSHTMLAssignment/VSHTMLAssignment/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var i = 0;
+var txt = 'Black Horse and A Cherry Tree';
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
